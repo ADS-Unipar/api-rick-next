13b9ba6b39662840efc951fd32930a4c4b9e92b0
OAllanWeb <allanweb019@gmail.com>
2024-11-11 22:06:57 +0000

    sobe aula

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import SearchBox from "@/components/SerarchBox";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
